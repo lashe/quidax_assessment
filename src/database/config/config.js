3ad640cb3207530c2959ec39d6bd825d759e2f0e
Toluwalashe <tee1alive@yahoo.com>
2022-02-18 14:25:46 +0100

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: "root",
-        password: "mysqlserver",
+        password: "password",
         database: "quidax_books",
         host: "127.0.0.1",
         dialect: "mysql",
